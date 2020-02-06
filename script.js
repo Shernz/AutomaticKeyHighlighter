22c032d16d18a7f8d9ba29be6df93ead2018453a
Sharanya Vijayalakshmi <kr.vijayalakshmy25@gmail.com>
2020-02-06 21:21:24 +0530

    Second Commit

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-// var $ = require("jQuery") require is not working well 
-$(document).ready(function(){ // if you want to use load ---- $(window).on('load', function() { ... });
-    $("article").addClass("highlight");
-})
+// // var $ = require("jQuery") require is not working well 
+// $(document).ready(function(){ // if you want to use load ---- $(window).on('load', function() { ... });
+//     $("article").addClass("highlight");
+// })
+
+
+// We want only the words so lets not meddle with jQuery
+
+var words = document.getElementsByTagName('p'); //reads p tags all good but not printing them?!
+console.log(words[0].innerText);
