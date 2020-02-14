459a5d3f5ff441bd2f9a702b50bccfc3785a36f8
Sharanya Vijayalakshmi <kr.vijayalakshmy25@gmail.com>
2020-02-14 21:52:30 +0530

    Word extraction

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 // // var $ = require("jQuery") require is not working well 
-// $(document).ready(function(){ // if you want to use load ---- $(window).on('load', function() { ... });
-//     $("article").addClass("highlight");
-// })
+$(function(){ // if you want to use load ---- $(window).on('load', function() { ... });
+    var words = []
+    $("body").children().not("script").each(function(i, ele) {
+        words.push($(ele).text().trim());
+    });
+    process(words)
+})
 
+function process(wordList) {
+    console.log(wordList) // 0th index contains the heading tags
+}
 
-// We want only the words so lets not meddle with jQuery
-
-var words = [];
-words.push(document.getElementsByTagName("P")[0].textContent); //reads p tags all good but not printing them?! -- resolved by changing the script location.
-console.log(words);
-
-/* To extract all the text -- store 
-        document.getElementsByTagName("P") seperately and access each tag by it's index.
-*/
-
-
+// Use TF-IDF for keyword detection.
+// You can't use the key-value[number] pair as you want to record both term and document frequency.
+// Hence you can use objects as value for the key, as arrays may complicate things in future.
