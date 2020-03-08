0870571d9b718ad580ea37fa8a15ab23820b6c62
Sharanya Vijayalakshmi <kr.vijayalakshmy25@gmail.com>
2020-03-08 12:24:28 +0530

    Updated

--- script.js
+++ script.js
@@ -1,27 +1,22 @@
-// // var $ = require("jQuery") require is not working well 
+
 $(function(){ // if you want to use load ---- $(window).on('load', function() { ... });
     var words = []
-    $("body").children().not("script", "code").each(function(i, ele) { // extracting the words under body tag 
+    $("body").children().not("script, code").each(function(i, ele) { // extracting the words under body tag 
         words = words.concat($(ele).text().trim().split(" ")); // pushing them to the array after trimming off the white space and splitting them into words 
     });
     process(words) // call the core function
 })
 
-// function compare(a, b) {
-//     return 
-// }
 
 function process(wordList) { // core function
-    let tfdf = {}, keywords = [], keys = []
-     // 0th index contains the heading tags
+    let tfdf = {}, keywords = [], keys = [], special = /[!@#$%^&*(){}\+\-.;:/*<>,''""]+/
     
     for (let i in wordList) {
-        if  (!/\d+/.test(wordList[i])) {
+        if  (!/\d+/.test(wordList[i]) &&  (!(special.test(wordList[i])))) {
             if ((wordList[i] !== undefined) && (!(wordList[i] in tfdf))) {  // calculate the occurance of each word tf : Term Frequency and df = document frequency... No of times the word occurs in all the documents here its always one
                 tfdf[wordList[i]] = {
                     "tf" : 1
                 }
-                // keys.push(wordList[i])
             }
             else {
                 tfdf[wordList[i]]["tf"] += 1
@@ -29,13 +24,26 @@ function process(wordList) { // core function
         }
     }
     
-    // console.log(tfdf)
     for (let w in tfdf) {
         let tf = tfdf[w].tf
         tfdf[w].score = tf * Math.abs(Math.log(1 / tf))
         if (tfdf[w].score === 0) keywords.push(w)
     }
-    console.log(keywords)
+    // console.log(tfdf)
+    highlighter(keywords)
+}
+
+function highlighter (words) {
+    // console.log(words)
+    // var Html = $('body').html()
+    // words.forEach(word => {
+    //     Html = Html.replace(word, `<span>${word}</span>`);
+    //     // $('body').html().replace(wo, "<span class = 'highlight'>" + word + "</span>"))
+    // });
+    // $('body').html(Html)
+    // $("span").addClass("highlight")
+    // console.log(Html)
+    $('body').highlight(words, {wordsOnly : true})
 }
 
 // Use TF-IDF for keyword detection.
