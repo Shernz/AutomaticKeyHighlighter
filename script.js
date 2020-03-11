ca08756e5490cdd3c666a1e761258e1799bba5f8
Sharanya Vijayalakshmi <kr.vijayalakshmy25@gmail.com>
2020-03-11 18:48:39 +0530

    Final

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-
-$(function(){ // if you want to use load ---- $(window).on('load', function() { ... });
+$(function(){ 
     var words = []
-    $("body").children().not("script, code").each(function(i, ele) { // extracting the words under body tag 
-        words = words.concat($(ele).text().trim().split(" ")); // pushing them to the array after trimming off the white space and splitting them into words 
+    $("body").children().not("script").each(function(i, ele) { 
+        words = words.concat($(ele).text().trim().split(" ")); 
     });
-    process(words) // call the core function
+    process(words) 
 })
 
 
-function process(wordList) { // core function
+function process(wordList) { 
     let tfdf = {}, keywords = [], keys = [], special = /[!@#$%^&*(){}\+\-.;:/*<>,''""]+/
     
     for (let i in wordList) {
         if  (!/\d+/.test(wordList[i]) &&  (!(special.test(wordList[i])))) {
-            if ((wordList[i] !== undefined) && (!(wordList[i] in tfdf))) {  // calculate the occurance of each word tf : Term Frequency and df = document frequency... No of times the word occurs in all the documents here its always one
+            if ((wordList[i] !== undefined) && (!(wordList[i] in tfdf))) {  
                 tfdf[wordList[i]] = {
                     "tf" : 1
                 }
@@ -29,24 +28,12 @@ function process(wordList) { // core function
         tfdf[w].score = tf * Math.abs(Math.log(1 / tf))
         if (tfdf[w].score === 0) keywords.push(w)
     }
-    // console.log(tfdf)
     highlighter(keywords)
 }
 
+
 function highlighter (words) {
-    // console.log(words)
-    // var Html = $('body').html()
-    // words.forEach(word => {
-    //     Html = Html.replace(word, `<span>${word}</span>`);
-    //     // $('body').html().replace(wo, "<span class = 'highlight'>" + word + "</span>"))
-    // });
-    // $('body').html(Html)
-    // $("span").addClass("highlight")
-    // console.log(Html)
     $('body').highlight(words, {wordsOnly : true})
 }
 
-// Use TF-IDF for keyword detection.
-// You can't use the key-value[number] pair as you want to record both term and document frequency.
-// Hence you can use objects as value for the key, as arrays may complicate things in future.
-// score = tf * log((no of docs) / (no of documents in which the word appeared))  for reducing the scale
+
